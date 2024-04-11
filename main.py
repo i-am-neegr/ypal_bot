10ef84b5477af67fa0239836807e019e834a5971
Artem Sergeev <martial.artem@gmail.com>
2024-04-11 17:22:28 +0300

    add main

---
+++ main.py
@@ -0,0 +1,11 @@
+from src.message_time import morning_random_moment, evening_random_moment
+from aiogram import Bot, Dispatcher
+
+# создаем бота и его диспетчера
+# bot = Bot(token="токен сюда ctrl + v")
+# dp = Dispatcher()
+
+morning_time = morning_random_moment("9:30", "11:45")
+evening_time = evening_random_moment("17:45", "20:30")
+
+print(morning_time)
---
+++ src/message_time.py
@@ -0,0 +1,44 @@
+import random
+from datetime import time, datetime
+
+
+def morning_random_moment(start_time: str, end_time: str) -> time:
+    start_hour, start_minutes = start_time.split(":")
+    end_hours, end_minutes = end_time.split(":")
+
+    start_time = time(int(start_hour), int(start_minutes), 0)
+    end_time = time(int(end_hours), int(end_minutes), 0)
+
+    random_hour = random.randint(start_time.hour, end_time.hour)
+    random_minute = random.randint(0, 59)
+    random_second = random.randint(0, 59)
+    random_microsecond = random.randint(0, 999999)
+
+    random_time = time(random_hour, random_minute, random_second, random_microsecond)
+
+    return random_time
+
+
+def evening_random_moment(start_time: str, end_time: str) -> time:
+    start_hour, start_minutes = start_time.split(":")
+    end_hours, end_minutes = end_time.split(":")
+
+    start_time = time(int(start_hour), int(start_minutes), 0)
+    end_time = time(int(end_hours), int(end_minutes), 0)
+
+    random_hour = random.randint(start_time.hour, end_time.hour)
+    random_minute = random.randint(0, 59)
+    random_second = random.randint(0, 59)
+    random_microsecond = random.randint(0, 999999)
+
+    random_time = time(random_hour, random_minute, random_second, random_microsecond)
+
+    return random_time
+
+
+def is_valid_message_time(sending_time: time) -> bool:
+    sending_time = str(sending_time.strftime('%H:%M'))
+    current_time = str(datetime.now().strftime('%H:%M'))
+    return current_time == sending_time
+
+
