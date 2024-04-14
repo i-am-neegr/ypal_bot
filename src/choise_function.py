d3c73025c80f77eb71160415dbf6c32bf78bbf03
Daniil Tihomirov <daniltihomirov946@gmail.com>
2024-04-14 22:38:24 +0300

    add dockstring

--- src/choise_function.py
+++ src/choise_function.py
@@ -3,5 +3,6 @@ from src.choise_text import random_motivation, random_phrases, random_compliment
 
 
 def random_function(list_function: list) -> str:
+    """берет рандом """
     function = choice(list_function)
     return function()
