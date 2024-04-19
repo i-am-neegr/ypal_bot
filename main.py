3ccd446d2acc6336acc2fb4db8ac3dde6b57d8b2
Artem Sergeev <martial.artem@gmail.com>
2024-04-19 19:05:57 +0300

    timer and delete evening time(one function for all times)

--- main.py
+++ main.py
@@ -1,9 +1,32 @@
-from src.message_time import morning_random_moment, evening_random_moment
+import asyncio
+
+from src.message_time import random_moment, is_valid_message_time
 from aiogram import Bot, Dispatcher
+from asyncio import sleep
 
 # создаем бота и его диспетчера
 # bot = Bot(token="токен сюда ctrl + v")
 # dp = Dispatcher()
 
-morning_time = morning_random_moment("9:30", "11:45")
-evening_time = evening_random_moment("17:45", "20:30")
+
+should_sleep = False
+
+async def timer():
+    global should_sleep
+    if should_sleep:
+        await sleep(5)
+    morning_time = random_moment("9:30", "11:00")
+    lunch_time = random_moment("13:00", "14:30")
+    evening_time = random_moment("19:00", "20:30")
+    print(morning_time)
+
+    should_sleep = True
+
+    return morning_time, lunch_time, evening_time
+
+async def main():
+    for i in range(5):
+        await timer()
+
+if __name__ == "__main__":
+    asyncio.run(main())
--- src/message_time.py
+++ src/message_time.py
@@ -2,7 +2,7 @@ import random
 from datetime import time, datetime
 
 
-def morning_random_moment(start_time: str, end_time: str) -> time:
+def random_moment(start_time: str, end_time: str) -> time:
     start_hour, start_minutes = start_time.split(":")
     end_hours, end_minutes = end_time.split(":")
 
@@ -19,21 +19,7 @@ def morning_random_moment(start_time: str, end_time: str) -> time:
     return random_time
 
 
-def evening_random_moment(start_time: str, end_time: str) -> time:
-    start_hour, start_minutes = start_time.split(":")
-    end_hours, end_minutes = end_time.split(":")
 
-    start_time = time(int(start_hour), int(start_minutes), 0)
-    end_time = time(int(end_hours), int(end_minutes), 0)
-
-    random_hour = random.randint(start_time.hour, end_time.hour)
-    random_minute = random.randint(0, 59)
-    random_second = random.randint(0, 59)
-    random_microsecond = random.randint(0, 999999)
-
-    random_time = time(random_hour, random_minute, random_second, random_microsecond)
-
-    return random_time
 
 
 def is_valid_message_time(sending_time: time) -> bool:
